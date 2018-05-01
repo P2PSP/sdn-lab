06456c8cdeacfdada117b153d1e80f9edc4bef0a
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-05-01 12:09:35 +0200

    working on installing flow

--- scrambling_p2p/scrambling_p2p.py
+++ scrambling_p2p/scrambling_p2p.py
@@ -28,7 +28,6 @@ class ScramblingP2P(app_manager.RyuApp):
                 (address.split(":")[0], int(address.split(":")[1]))
             )
         print("List of the team:\n{}".format(self.peers_list))
-        # self.mac_to_port = {}
         self.scrambling_list = dict(
             zip(self.peers_list, sample(self.peers_list, len(self.peers_list)))
         )
@@ -42,8 +41,7 @@ class ScramblingP2P(app_manager.RyuApp):
 
         # install the table-miss flow entry. (OF 1.3+)
         match = parser.OFPMatch()
-        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
-                                          ofproto.OFPCML_NO_BUFFER)]
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
@@ -60,6 +58,7 @@ class ScramblingP2P(app_manager.RyuApp):
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
+        print("Flow installed")
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
@@ -73,11 +72,11 @@ class ScramblingP2P(app_manager.RyuApp):
         ip_pkt = pkt.get_protocol(ipv4.ipv4)
         udp_pkt = pkt.get_protocol(udp.udp)
 
-        if udp_pkt and 1 == 0:
+        if udp_pkt:
             dst_peer = (ip_pkt.dst, udp_pkt.dst_port)
             if dst_peer in self.scrambling_list:
                 self.ip_to_mac[ip_pkt.src] = eth_pkt.src
-                self.mac_to_port[eth_pkt.src] = msg.in_port
+                self.mac_to_port[eth_pkt.src] = in_port
                 if self.scrambling_list[dst_peer][0] in self.ip_to_mac:
                     dst_mac = self.ip_to_mac[
                         self.scrambling_list[dst_peer][0]
@@ -87,23 +86,29 @@ class ScramblingP2P(app_manager.RyuApp):
                     dst_mac = 'ff:ff:ff:ff:ff:ff'
                     dst_port = ofp.OFPP_FLOOD
 
-                print("Message to", dst_peer, "from", ip_pkt.src)
-                match = dp.ofproto_parser.OFPMatch(
-                    in_port=msg.in_port,
-                    nw_dst=dst_peer[0],
-                    tp_dst=dst_peer[1])
+                print("Message to", dst_peer, "from", ip_pkt.src, "sent to",
+                      self.scrambling_list[dst_peer][0], "via", dst_mac)
+                match = dp.ofproto_parser.OFPMatch(in_port=in_port)
+                    #ipv4_dst=dst_peer[0],
+                    #udp_dst=dst_peer[1])
                 actions = []
-                actions.append(ofp_parser.OFPActionSetDlDst(dst_mac))
-                actions.append(ofp_parser.OFPActionSetNwDst(
-                    self.scrambling_list[dst_peer][0]))
+                actions.append(ofp_parser.OFPActionSetField(eth_dst=dst_mac))
+                actions.append(ofp_parser.OFPActionSetField(
+                    ipv4_dst=self.scrambling_list[dst_peer][0]))
                 actions.append(ofp_parser.OFPActionOutput(port=dst_port))
                 # TO-DO: Use self.add_flow()
-                out = ofp_parser.OFPFlowMod(
-                    command=ofp.OFPFC_ADD,
-                    datapath=dp, buffer_id=msg.buffer_id,
-                    actions=actions, match=match, hard_timeout=30,
-                    idle_timeout=10)
-                dp.send_msg(out)
+                if msg.buffer_id != ofp.OFP_NO_BUFFER:
+                    self.add_flow(dp, 1, match, actions, msg.buffer_id)
+                    print("BUFFERID", msg.buffer_id)
+                    return
+                else:
+                    self.add_flow(dp, 1, match, actions)
+                #out = ofp_parser.OFPFlowMod(
+                #    command=ofp.OFPFC_ADD,
+                #    datapath=dp, buffer_id=msg.buffer_id,
+                #    actions=actions, match=match, hard_timeout=30,
+                #    idle_timeout=10)
+                #dp.send_msg(out)
             else:
                 actions = [ofp_parser.OFPActionOutput(ofp.OFPP_FLOOD)]
         else:
