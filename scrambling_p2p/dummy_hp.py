8a959815e84c44fc1ba5a26a601a014c5f4967d6
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-05-08 13:26:56 +0200

    dummy honest peer added

---
+++ scrambling_p2p/dummy_hp.py
@@ -0,0 +1,52 @@
+import socket
+import argparse
+
+
+class DummyHP():
+
+    def __init__(self, port, splitter, peer_list):
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', self.port))
+        self.splitter = splitter
+        self.peer_list = peer_list
+
+    def receive(self):
+        data, address = self.sock.recvfrom(1)
+        return (data, address)
+
+    def send(self, data, address):
+        self.sock.sendto(data, address)
+
+    def run(self):
+        while True:
+            data, address = self.receive()
+            print("\n{} received from {}".format(data, address))
+            if address == self.splitter:
+                for peer in self.peer_list:
+                    self.send(data, peer)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int,
+                        help="UDP port used by peers")
+    parser.add_argument("-s", "--splitter", type=str,
+                        help="Splitter address")
+    parser.add_argument("-z", "--size", type=int,
+                        help="Team size (without splitter)")
+    args = parser.parse_args()
+
+    peer_list = []
+    for p in range(1, args.size+1):
+        peer_list.append("10.0.0."+str(p))
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect((args.splitter, 1))
+    local_ip_address = s.getsockname()[0]
+
+    print("Local IP {}".format(local_ip_address))
+    peer_list.remove(local_ip_address)
+    print("\nPeer List:{}".format(peer_list))
+    peer = DummyHP(args.port, args.splitter, peer_list)
+    peer.run()
