728e8fb3c441062304fc26ec6d9610b821c6a466
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-08-05 07:15:30 +0200

    .

--- scrambling_p2p/probability.py
+++ scrambling_p2p/probability.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-import sys
 
+import sys
 
 if __name__ == "__main__":
     team_size = int(sys.argv[1])
     _round = int(sys.argv[2])
-    probability = (((team_size - 2) / (team_size - 1)) **
-                   (_round - 1)) * (1 / (team_size - 1))
-    print("Probability of attacking to the TP in the round {} "
+    prob = 0
+    for i in range(0,_round):
+        probability = (((team_size - 2) / (team_size - 1)) **
+                       (i)) * (1 / (team_size - 1))
+        print("Probability of attacking to the TP in the round {} "
+              "in a team of {} is {}"
+              .format(_round, team_size, probability))
+        prob += probability
+    print("Probability of attacking to the TP after {} rounds "
           "in a team of {} is {}"
-          .format(_round, team_size, probability))
+          .format(_round, team_size, prob))
