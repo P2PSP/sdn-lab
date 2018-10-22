b18bf6975df590573abede1893fe93b39f1eedae
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-10-22 13:27:55 +0200

    simulate period changes

--- scrambling_p2p/period.sh
+++ scrambling_p2p/period.sh
@@ -1,25 +1,12 @@
 #!/bin/bash
 
-samples=30
+samples=100
 
-for ((t=1; t <= 10; t=t+1))
+for (( period=1; period <= 16; period*=2))
 do
-    
-    for (( i=0; i < $samples; ++i ))
+    for (( i=1; i <= $samples; ++i ))
     do
-	python3 fast_sim.py -z 100 -t 3 -p 1 | grep -Eo "Detected in round [0-9]{1,}" | grep -Eo "[0-9]{1,}" >> period_$t.out
-    done
-
-    #cat fixed_$t.out | sort | uniq -c > fixed_count_$t.dat
-    avg=$(awk '{ total += $1; count++ } END { print total/count }' fixed_$t.out)
-    echo -e $t "\t" $avg >> fixed.dat
-
-    for (( i=0; i < $samples; ++i ))
-    do
-	./run.sh $t 12345 1 1 | grep -Eo 'MP detected in round [0-9]{1,}' | grep -Eo '[0-9]{1,}' >> var_$t.out
-    done
-
-    #cat var_$t.out | sort | uniq -c > var_count_$t.dat
-    avg=$(awk '{ total += $1; count++ } END { print total/count }' var_$t.out)
-    echo -e $t "\t" $avg >> var.dat
+	python3 fast_sim.py -z 100 -t 3 -p $period | grep -Eo "Detected in round [0-9]{1,}" | grep -Eo "[0-9]{1,}" >> period_$period.out
+done
+    sort -n period_$period.out | uniq -c | tr -s ' ' | awk '{s+=$1; print $2, $1, s}' > period_$period.dat
 done
