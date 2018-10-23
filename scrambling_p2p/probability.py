7fbd83ad26e630a3b26067609a1c5190cb2fec4e
Cristóbal Medina López <cristobalmedinalopez@gmail.com>
2018-10-23 10:56:16 +0200

    formating...

--- scrambling_p2p/probability.py
+++ scrambling_p2p/probability.py
@@ -10,9 +10,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 3:
         s = int(sys.argv[3])
     prob = 0
-    for i in range(1,_round+1):
+    for i in range(1, _round+1):
         probability = (((team_size - 2) / (team_size - 1)) **
-                       (math.ceil(i/s)-1)) * (0**(i%s) / (team_size - 1))
+                       (math.ceil(i/s)-1)) * (0 ** (i % s) / (team_size - 1))
         print("Probability of attacking to the TP in the round {} "
               "in a team of {} is {}"
               .format(i, team_size, probability))
